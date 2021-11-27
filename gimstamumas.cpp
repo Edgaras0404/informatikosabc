060df34ceb23900af6c33f8bcbf99a818b1dfb28
Edgaras0404 <edgaras.ignas@gmail.com>
2021-11-27 03:39:08 -0800

    gimstamumas

---
+++ gimstamumas.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+
+void input ( int &n, std::string lName[], std::string fName[], char gender[], int year[], int month[], int day[] ) {
+    std::ifstream in("Gimstamumas.txt");
+    in >> n;
+    for (int i = 0; i < n; i++)
+    {
+        in.ignore(80, '\n');
+        char name[13];
+        in.read(name, 12);
+        lName[i] = name;
+        in.ignore(1,'\n');
+        in.read(name, 12);
+        fName[i] = name;
+        in >> gender[i] >> year[i] >> month[i] >> day[i];
+    }
+
+    in.close(); 
+}
+
+
+int main () {
+    int n;  
+    std::string fName[10000];
+    std::string lName[10000];
+    char gender[10000];
+    int year[10000];
+    int month[10000];
+    int day[10000];
+
+    input ( n, lName, fName, gender, year, month, day );
+
+    int males = 0, females = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (gender[i] == 'v')
+        {
+            males++; 
+        } else if (gender[i] == 'm')
+        {
+            females++;
+        }
+         
+    }
+    std::string mpfName, mpmName;
+    int mpfC = 0, mpmC = 0;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int reocc = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (fName[i] == fName[j])
+            {
+                reocc++; 
+            }
+        }
+        if (gender[i] == 'v')
+        {
+            if (reocc > mpmC)
+            {
+                mpmC = reocc;
+                mpmName = fName[i];
+            } else if (reocc == mpmC)
+            {
+                if (mpmName > fName[i])
+                {
+                    mpmName = fName[i]; 
+                }
+            }
+        } else {
+            if (reocc > mpfC)
+            {
+                mpfC = reocc;
+                mpfName = fName[i];
+            } else if (reocc == mpfC) {
+                if (mpfName > fName[i])
+                {
+                    mpfName = fName[i]; 
+                }
+            }
+        }
+    }
+
+    int months[12] = { 0 };
+    for (int i = 0; i < 12; i++)
+    {
+        months[month[i]-1]++;
+    }
+
+    std::ofstream out("Rezgimstamumas.txt");
+    out << "Berniukai: " << males << std::endl;
+    out << mpmName << std::endl;
+    out << "Mergaites: " << females << std::endl;
+    out << mpfName << std::endl;
+    for (int i = 0; i < 12; i++)
+    {
+        out << months[i] << " ";
+    }
+    out << std::endl;  
+    
+    
+
+
+    
+    return 0;
+}
